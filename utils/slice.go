f634a44a829072235c8f6ecdfa93058f623894f7
Brian Shannan <brianshannan@gmail.com>
2024-03-22 00:01:54 -0500

    cleanup

--- utils/slice.go
+++ utils/slice.go
@@ -1,15 +1,9 @@
 package utils
 
-import "strconv"
-
 func SliceMap[T, U any](input []T, fn func(T) U) []U {
 	output := make([]U, len(input))
 	for i, inputVal := range input {
 		output[i] = fn(inputVal)
 	}
 	return output
-}
-
-func IntSliceToStringSlice(ints []int) []string {
-	return SliceMap(ints, strconv.Itoa)
 }
